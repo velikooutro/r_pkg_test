e2b5b7ff18eba6ffb07c78da6462d7e60e385465
veliko.outro <veliko.outro@gmail.com>
2014-12-21 14:34:53 -0500

    final adds

---
+++ R/test_pkg.R
@@ -0,0 +1,8 @@
+test_function <- function(love=TRUE){
+    if(love==TRUE){
+        print("I love to code!")
+    }
+    else {
+        print("I am not a cool person.")
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-r_pkg_test
+pkgtest
 ==========
